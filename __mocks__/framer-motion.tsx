b87f9864bc95e9b40d655f91f714e87ae0f42923
Diego Escobar <diego.e@me.com>
2026-02-25 00:56:18 -0600

    chore: filter animation props from framer-motion mock to silence DOM warnings

--- __mocks__/framer-motion.tsx
+++ __mocks__/framer-motion.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+const motionProps = new Set([
+  'initial', 'animate', 'exit', 'variants', 'transition',
+  'whileHover', 'whileTap', 'whileFocus', 'whileInView',
+  'viewport', 'layout', 'layoutId', 'onAnimationStart',
+  'onAnimationComplete', 'drag', 'dragConstraints',
+])
+
 const motion = new Proxy({}, {
   get: (_target, prop: string) => {
     const Component = React.forwardRef(
-      ({ children, ...props }: React.HTMLAttributes<HTMLElement>, ref: React.Ref<HTMLElement>) =>
-        React.createElement(prop, { ...props, ref }, children)
+      ({ children, ...props }: React.HTMLAttributes<HTMLElement>, ref: React.Ref<HTMLElement>) => {
+        const domProps = Object.fromEntries(
+          Object.entries(props).filter(([key]) => !motionProps.has(key))
+        )
+        return React.createElement(prop, { ...domProps, ref }, children)
+      }
     )
     Component.displayName = prop
     return Component
